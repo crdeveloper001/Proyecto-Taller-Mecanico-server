90e75a001801c3eb4e96d9638f1ba23186ce363f
claugh23 <claudiogh33@gmail.com>
2024-06-27 22:34:51 -0600

    update jar and job controller put request

--- src/main/java/com/example/tallermecanicoserverv2/Controllers/JobsController.java
+++ src/main/java/com/example/tallermecanicoserverv2/Controllers/JobsController.java
@@ -16,35 +16,53 @@ public class JobsController {
 
     @Autowired
     private JobsService service;
+
     @GetMapping
     public List<JobsDTO> getAllJobs() {
         return service.GetAllJobs();
     }
+
     @GetMapping("/Search/{status}")
     public List<JobsDTO> getJobsByStatus(@PathVariable String status) {
         return service.SearchOneJobByStatus(status);
 
     }
+
     @PostMapping()
     public ResponseEntity<?> addJob(@RequestBody JobsDTO new_job) {
 
         switch (service.AddNewJob(new_job)) {
             case "CREATED":
-                return new ResponseEntity<>(HttpStatus.CREATED);
+                return new ResponseEntity<>("CREATED", HttpStatus.CREATED);
             case "NOT_CREATED":
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
+                return new ResponseEntity<>("NOT_CREATED", HttpStatus.CONFLICT);
             default:
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>("BAD_RESPONSE", HttpStatus.BAD_REQUEST);
         }
 
     }
+
     @PutMapping()
     public ResponseEntity<?> updateJob(@RequestBody JobsDTO update) {
-        return new ResponseEntity<>(service.UpdateJobByStatus(update), HttpStatus.OK);
+
+        switch (update.getJob_Status()) {
+            case "In Progress":
+                update.setJob_Status("In Progress");
+                service.UpdateJobByStatus(update);
+                return new ResponseEntity<>("Job " + update.get_id() + " Moved to In Progress", HttpStatus.OK);
+
+            case "Completed":
+                update.setJob_Status("Completed");
+                service.UpdateJobByStatus(update);
+                return new ResponseEntity<>("Job " + update.get_id() + " Moved to Completed", HttpStatus.OK);
+            default:
+                return new ResponseEntity<>("An error occur for update this job", HttpStatus.BAD_REQUEST);
+        }
 
     }
+
     @DeleteMapping("/Delete/{jobId}")
     public ResponseEntity<?> deleteJob(@PathVariable String jobId) {
-        return new ResponseEntity<>(service.DeleteJobById(jobId),HttpStatus.OK);
+        return new ResponseEntity<>(service.DeleteJobById(jobId), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/tallermecanicoserverv2/TallerMecanicoServerv2Application.java
+++ src/main/java/com/example/tallermecanicoserverv2/TallerMecanicoServerv2Application.java
@@ -1,18 +1,15 @@
 package com.example.tallermecanicoserverv2;
 
-import jakarta.servlet.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.HttpMethod;
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"com.example.tallermecanicoserverv2.Repositories"})
 public class TallerMecanicoServerv2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TallerMecanicoServerv2Application.class, args);
-
+		System.out.println("API SERVER STARTED");
 	}
 
 }
