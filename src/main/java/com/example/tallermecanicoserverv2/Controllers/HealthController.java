693810ceb600d3df58720d951be10252a5cba035
crdeveloper001 <crdevelopers50@gmail.com>
2026-02-21 09:30:27 -0600

    .

--- src/main/java/com/example/tallermecanicoserverv2/Controllers/HealthController.java
+++ src/main/java/com/example/tallermecanicoserverv2/Controllers/HealthController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin(allowedHeaders = "*", origins = "*")
-@RequestMapping("api/v1/APIHealth")
+@RequestMapping("api/v1/Health")
 public class HealthController {
 
     @GetMapping()
